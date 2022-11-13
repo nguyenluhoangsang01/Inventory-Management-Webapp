44168171caf8f0c91fc62f04a561800c55033d4e
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-11-13 09:54:08 +0700

    add verifyToken to logout, getAllUsers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/users.js";
+import {
+  getAllUsers,
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleWares/auth.js";
 
 const router = express.Router();
 
@@ -16,6 +22,11 @@ router.post("/login", loginUser);
 // @route GET api/users/logout
 // @desc Logout user
 // @access Private
-router.get("/logout", logoutUser);
+router.get("/logout", verifyToken, logoutUser);
+
+// @route GET api/users
+// @desc Get all users
+// @access Private
+router.get("/", verifyToken, getAllUsers);
 
 export default router;
